6fb3ab11a8b8fd700e57f87f2962255b2a214c00
Shivane Rana <shivanerana2004@gmail.com>
2024-10-09 21:21:54 +0530

    basic styling done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import "./style.css"
+import "./style.css";
+import "./asset/Gilroy/stylesheet.css";
+
+const theme = document.querySelector(".theme");
+
+theme.addEventListener("click",() => {
+    console.log("theme button was clicked");
+    document.documentElement.classList.toggle("dark");
+})
