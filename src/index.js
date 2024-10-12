88c062d795746adcb1e38992bcbca86fa7ce6e49
Shivane Rana <shivanerana2004@gmail.com>
2024-10-12 14:15:29 +0530

    project overhaul

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import "./style.css";
 import "./asset/Gilroy/stylesheet.css";
-import { project } from "./modules/project";
 
+
+const expand = document.querySelector(".expand");
 const addNewProject = document.querySelector(".addNewProject");
 const projectContainer = document.querySelector(".projectContainer");
-
-document.addEventListener("DOMContentLoaded", () => {
-  // render elements present in the project list
-  project.displayProject(projectContainer);
-});
-
-// add new project
-addNewProject.addEventListener("click", () => {
-  let result = project.askProjectName();
-  if (result === true) {
-    console.log(project.getList());
-    projectContainer.textContent = "";
-    project.displayProject(projectContainer, project.getList());
-  } else {
-    console.log("no changes were made to the project list");
-  }
-});
