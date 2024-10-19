256ca04bcff3fd8fc58d74003b7528b689f097d7
Shivane Rana <shivanerana2004@gmail.com>
2024-10-19 21:04:34 +0530

    addTodoOrNotes created

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const navBar = document.querySelector(".navBar");
 const mainBar = document.querySelector(".mainBar");
 const addNewProject = document.querySelector(".addNewProject");
 const projectContainer = document.querySelector(".projectContainer");
+const addTodoOrNotes = document.querySelector(".add");
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -27,3 +28,5 @@ theme.addEventListener("click", () => {
 addNewProject.addEventListener("click", () => {
   projectLogic.finale(projectContainer);
 });
+
+addTodoOrNotes.style.backgroundColor = "red";
