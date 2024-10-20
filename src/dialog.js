59978272eddba4439c1de97f2ae66e34ecbf155f
Shivane Rana <shivanerana2004@gmail.com>
2024-10-20 12:21:46 +0530

    all classed applied to todo elements

--- src/dialog.js
+++ src/dialog.js
@@ -15,34 +15,58 @@ export const renderDefaultDialog = (function () {
   //   these button are common in both tab
   const close = document.createElement("button");
   close.textContent = "Close";
+  close.classList.add("close");
   const clearAll = document.createElement("button");
   clearAll.textContent = "Clear All";
+  clearAll.classList.add("clearAll");
   const confirm = document.createElement("button");
   confirm.textContent = "Confirm";
+  confirm.classList.add("confirm");
 
   // todo files
   const todoArea = document.createElement("div");
+
   const th1 = document.createElement("h1");
   th1.textContent = "Add Todo";
+
   const tTitle = document.createElement("input");
+  tTitle.setAttribute("placeholder", "add title");
+  tTitle.classList.add("tTitle");
+
   const tDate = document.createElement("input");
+  tDate.setAttribute("type", "date");
+  tDate.classList.add("tDate");
+
   const tDescription = document.createElement("textarea");
+  tDescription.setAttribute("placeholder", "add description");
+  tDescription.classList.add("tDescription");
+
   const tPriority = document.createElement("h3");
   tPriority.textContent = "Priority:";
+  tPriority.classList.add("tPriority");
+
   const low = document.createElement("button");
   low.textContent = "Low";
   const medium = document.createElement("button");
   medium.textContent = "Medium";
   const high = document.createElement("button");
   high.textContent = "High";
+
   const tProject = document.createElement("h3");
   tProject.textContent = "Project:";
+  tProject.classList.add("tProject");
+
   const targetProject = document.createElement("select");
+  const option1 = document.createElement("option");
+  option1.textContent = "All";
+  targetProject.append(option1);
 
   // notes files
   const notesArea = document.createElement("div");
+  notesArea.classList.add();
   const nh1 = document.createElement("h1");
   nh1.textContent = "Add Notes";
+
   const nTitle = document.createElement("input");
   const nDescription = document.createElement("input");
 
@@ -61,25 +85,14 @@ export const renderDefaultDialog = (function () {
   dialog.append(wrapper);
   wrapper.append(buttonHolder, todoArea, notesArea);
 
+  todoArea.append(th1, close.cloneNode(true), tTitle);
+  todoArea.append(tDate, tDescription, tPriority);
+  todoArea.append(tProject, clearAll.cloneNode(true), confirm.cloneNode(true));
+
   buttonHolder.append(showTodo, showNotes);
-
-  todoArea.append(
-    th1,
-    close,
-    tTitle,
-    tDate,
-    tDescription,
-    tPriority,
-    low,
-    medium,
-    high,
-    tProject,
-    targetProject,
-    clearAll,
-    confirm,
-  );
-
   notesArea.append(nh1, close, nTitle, nDescription, clearAll, confirm);
+  tPriority.append(low, medium, high);
+  tProject.append(targetProject);
 
   document.body.append(dialog);
 
