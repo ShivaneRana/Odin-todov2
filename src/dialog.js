ed1909cbebec4ff72f2b28c617362f9aefd3419f
Shivane Rana <shivanerana2004@gmail.com>
2024-10-20 11:50:03 +0530

    text Content added to various varaible

--- src/dialog.js
+++ src/dialog.js
@@ -5,28 +5,44 @@ export const renderDefaultDialog = (function () {
   const dialog = document.createElement("dialog");
   const wrapper = document.createElement("div");
   const buttonHolder = document.createElement("div");
+
+  //button for switching between todo and notes tab
   const showTodo = document.createElement("button");
+  showTodo.textContent = "-> Todo";
   const showNotes = document.createElement("button");
+  showNotes.textContent = "Notes";
+
+  //   these button are common in both tab
   const close = document.createElement("button");
+  close.textContent = "Close";
   const clearAll = document.createElement("button");
+  clearAll.textContent = "Clear All";
   const confirm = document.createElement("button");
+  confirm.textContent = "Confirm";
 
   // todo files
   const todoArea = document.createElement("div");
   const th1 = document.createElement("h1");
+  th1.textContent = "Add Todo";
   const tTitle = document.createElement("input");
   const tDate = document.createElement("input");
   const tDescription = document.createElement("textarea");
   const tPriority = document.createElement("h3");
+  tPriority.textContent = "Priority:";
   const low = document.createElement("button");
+  low.textContent = "Low";
   const medium = document.createElement("button");
+  medium.textContent = "Medium";
   const high = document.createElement("button");
+  high.textContent = "High";
   const tProject = document.createElement("h3");
+  tProject.textContent = "Project:";
   const targetProject = document.createElement("select");
 
   // notes files
   const notesArea = document.createElement("div");
   const nh1 = document.createElement("h1");
+  nh1.textContent = "Add Notes";
   const nTitle = document.createElement("input");
   const nDescription = document.createElement("input");
 
